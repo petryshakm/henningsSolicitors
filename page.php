a3d06b9859b2db17d04707ec889dd86abd63aa92
petryshakm <johndoe@example.com>
2019-01-21 16:51:52 +0200

    final

--- page.php
+++ page.php
@@ -1,16 +1,9 @@
-<?php get_header(); ?>
-		<div class="section-inner">
-			<div class="container-fluid">
-				<div class="row">
-					<div class="col-lg-12">
-						<?php if(have_posts()) : ?>
-						<?php while(have_posts()) : the_post(); ?>	
-						<?php the_title( '<h1 class = "title">', '</h1>', true ); ?>
-						<?php the_content(); ?>
-						<?php endwhile; ?>
-						<?php endif; ?>
-					</div>
-				</div>
-			</div>
-		</div>
-<?php get_footer(); ?>
+<?php /*Template Name: About us*/ ?>
+<?php get_template_part( 'template_parts/global/header' ); ?>
+
+<?php get_template_part( 'template_parts/global/page-title' ); ?>
+
+<?php get_template_part( 'template_parts/default-page/page-content' ); ?>
+
+<?php get_template_part( 'template_parts/global/footer' ); ?>
+
