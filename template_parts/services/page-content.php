0464671abcd243926a99b868db7f81ec736a40ce
Maksym <petryshakm@gmail.com>
2019-03-01 18:18:44 +0200

    services corrections

--- template_parts/services/page-content.php
+++ template_parts/services/page-content.php
@@ -15,7 +15,7 @@
 							'post_type' 		=> 'post',
 							'post_status' 		=> 'publish',
 							'posts_per_page' 	=> -1,
-							'orderby'			=> 'date',
+							'orderby'			=> 'menu_order',
 							'order'				=> 'ASC',
 							'category_name'		=> 'services'
 
